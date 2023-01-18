9e37387d0f24be9a403fd34baf04db10c0b53622
NightmaresBot <122958902+NightmaresBot@users.noreply.github.com>
2023-01-18 11:12:24 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6435225"))
-    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = "8364231"
+    API_HASH = "4e984ea35f854762dcde906dce426c2d"
+    BOT_TOKEN = "5858527916:AAHh-nuj0AkVJ9z239LtJ4t0V_eXr64nTK0"
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    BOT_USERNAME = "@NxghtmaresXopera_bot"
     SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
     CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
